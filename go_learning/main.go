7511594c7c1f783f6f30e43b644da4127efecca1
Yuriy Andreev <andreyev.yury@gmail.com>
2024-04-07 21:24:05 +0300

    go learn switch-case

--- go_learning/main.go
+++ go_learning/main.go
@@ -1,7 +1,19 @@
-package main
+/* package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Github")
+	fmt.Println(sWag(4))
 }
+
+func sWag(a int) int {
+	var b int
+	switch b {
+	case b%2 == 0:
+		return (b)
+	case b%2 == 1:
+		return (b + 1)
+
+	}
+} */
+//Продолжаем обучаться switch-case
